479e88613e93a1f97e53420d5fff4eedeec5f528
Nisarat-A <noansrnn@gmail.com>
2023-08-17 22:34:33 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,25 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="mb-1 font-mono text-4xl text-gray-100 md:text-6xl">
-        hi, I'm <br className="block md:hidden" />
-        <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform  text-yellow-100 ">
-          Nisarat
+        hi, I&apos;m <br className="block md:hidden" />
+        <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform text-yellow-500 text-lg md:text-6xl md:leading-none md:h">
+          Nisarat 👋
         </span>
         <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"></span>
       </h1>
-      <div className="text-xl font-semibold md:text-3xl">
-        " The world is one big data problem. - Andrew McAfee"
+      <div className="text-xl font-semibold md:text-3xl mt-4">
+        developer by choice and designer for fun
       </div>
 
-      
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center mt-8">
         <Image
           src={img_noey}
           alt="Picture of the author"
           layout="responsive"
           width={375} // Set the desired width for your smaller image
           height={667} // Set the corresponding height to maintain aspect ratio
-          className="mx-auto rounded-full sm:w-1/2 md:w-3/4 lg:w-full space-x-4"
+          className="mx-auto rounded-full sm:w-1/2 md:w-3/4 lg:w-full"
         />
-        <h1 className="text-4xl font-bold mt-8">Nisarat-A</h1>
-        <p className="text-xl mt-4">Data girl</p>
       </div>
     </main>
   )
