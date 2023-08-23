2321e4538024db3952aa9e2e5d659ba666a83c08
Nisarat-A <noansrnn@gmail.com>
2023-08-24 03:02:06 +0700

    align text and photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,42 +6,54 @@ import InterestCard from './components/InterestCard'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="mb-1 font-mono text-4xl text-gray-100 md:text-6xl">
-        hi, I&apos;m <br className="block md:hidden" />
-        <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform text-yellow-500 text-lg md:text-6xl md:leading-none md:h">
-          Nisarat 👋
-        </span>
-        <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"></span>
-      </h1>
-      <div className="text-xl font-semibold md:text-3xl mt-4">
-        data by it self is useless . data is only useful if you apply it .
-      </div>
-
-      <div className="flex flex-col items-center justify-center mt-8">
-        <Image
-          src={img_noey}
-          alt="Picture of the author"
-          layout="responsive"
-          width={375} // Set the desired width for your smaller image
-          height={667} // Set the corresponding height to maintain aspect ratio
-          className="mx-auto rounded-full sm:w-1/2 md:w-3/4 lg:w-full"
-        />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-        <InterestCard
-          title="Financial Markets"
-          description="I enjoy studying financial markets and analyzing trends to make informed decisions."
-        />
-        <InterestCard
-          title="Data-Driven Decisions"
-          description="I believe in the power of data to drive strategic decisions in various domains."
-        />
-        <InterestCard
-          title="Economic Analysis"
-          description="Understanding economic factors helps me interpret data and its implications."
-        />
-        {/* Add more InterestCard components for each interest */}
-      </div>
+      <div>
+      <section className="bg-brand-accent text-white py-16">
+        <div className="container mx-auto flex items-center justify-between p-24">
+          <div className="w-2/3">
+            <h1 className="mb-1 font-mono text-4xl md:text-6xl">
+              hi, I&apos;m <br className="block md:hidden" />
+              <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-yellow-500 text-lg md:text-6xl md:leading-none md:h">
+                Nisarat 👋
+              </span>
+            </h1>
+            <div className="text-xl font-semibold md:text-3xl mt-4">
+              data by itself is useless. data is only useful if you apply it.
+            </div>
+          </div>
+          <div className="w-1/3 flex justify-center">
+            <Image
+              src={img_noey}
+              alt="Picture of the author"
+              layout="responsive"
+              width={375}
+              height={667}
+              className="mx-auto rounded-full"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="py-16">
+        <div className="container mx-auto">
+          <h2 className="text-3xl font-bold mb-8">My Interest Areas</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <InterestCard
+              title="Financial Markets"
+              description="I enjoy studying financial markets and analyzing trends to make informed decisions."
+            />
+            <InterestCard
+              title="Data-Driven Decisions"
+              description="I believe in the power of data to drive strategic decisions in various domains."
+            />
+            <InterestCard
+              title="Economic Analysis"
+              description="Understanding economic factors helps me interpret data and its implications."
+            />
+            {/* Add more InterestCard components for each interest */}
+          </div>
+        </div>
+      </section>
+      {/* More content and components */}
+    </div>
     </main>
   )
 }
